582fb18f044ecbc6a8bb1c1b892e86817da2c64f
Gareth Johns <info@garethjohnsdesign.com>
2020-02-08 10:24:13 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vanessa Coyle
+
+Site for Vanessa Coyle. Please go to [www.vanessacoyle.com](https://www.vanessacoyle.com) in case you want to take a look at the website.
+
+* * *
+
+Copyright (c) 2020 Gareth Johns Design + Development – info@garethjohnsdesign.com
---
+++ storage/craft-copy-transfer.sql
@@ -0,0 +1,43 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for osx10.15 (x86_64)
+--
+-- Host: 127.0.0.1    Database: 813studio
+-- ------------------------------------------------------
+-- Server version	5.7.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `assetindexdata`
+--
+
+DROP TABLE IF EXISTS `assetindexdata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `assetindexdata` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sessionId` varchar(36) NOT NULL DEFAULT '',
+  `volumeId` int(11) NOT NULL,
+  `uri` text,
+  `size` bigint(20) unsigned DEFAULT NULL,
+  `timestamp` datetime DEFAULT NULL,
+  `recordId` int(11) DEFAULT NULL,
+  `inProgress` tinyint(1) DEFAULT '0',
+  `completed` tinyint(1) DEFAULT '0',
+  `dateCreated` datetime NOT NULL,
+  `dateUpdated` datetime NOT NULL,
+  `uid` char(36) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `assetindexdata_sessionId_volumeId_idx` (`sessionId`,`volumeId`),
+  KEY `assetindexdata_volumeId_idx` (`volumeId`),
+  CONSTRAINT `assetindexdata_volumeId_fk` FOREIGN KEY (`volumeId`) REFERENCES `volumes` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
