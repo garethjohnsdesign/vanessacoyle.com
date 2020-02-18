80cdb193cae94f02fd981c66fcd0d84bd453bc24
Gareth Johns <garethjohns@Gareths-iMac.local>
2020-02-18 19:06:37 +0000

    Cache Bust

--- config/general.php
+++ config/general.php
@@ -44,7 +44,7 @@ return [
         'enableTemplateCaching' => false,
 
         // Set the environmental variables
-        'staticAssetsVersion' => 2,
+        'staticAssetsVersion' => 3,
         
     ],
 
